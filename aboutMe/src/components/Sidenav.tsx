4b3a02f4707a80c79e33f83614b3f7eec9be2d50
ohno <TheDrop@gmx.net>
2024-06-12 11:51:28 +0200

    about me part zugefügt

--- aboutMe/src/components/Sidenav.tsx
+++ aboutMe/src/components/Sidenav.tsx
@@ -17,6 +17,7 @@ const Sidenav = () => {
   return (
     <div>
       <AiOutlineMenu
+        size={24}
         onClick={handleNav}
         className="fixed top-4 right-4 z-[99] md:hidden"
       />
@@ -29,6 +30,14 @@ const Sidenav = () => {
           >
             <AiOutlineHome size={20} />
             <span className="pl-4">Home</span>
+          </a>
+          <a
+            onClick={handleNav}
+            href="#aboutme"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <BsPerson size={20} />
+            <span className="pl-4">About me</span>
           </a>
           <a
             onClick={handleNav}
@@ -46,14 +55,7 @@ const Sidenav = () => {
             <AiOutlineProject size={20} />
             <span className="pl-4">Projects</span>
           </a>
-          <a
-            onClick={handleNav}
-            href="#main"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
-          >
-            <BsPerson size={20} />
-            <span className="pl-4">Resume</span>
-          </a>
+
           <a
             onClick={handleNav}
             href="#contact"
@@ -73,6 +75,12 @@ const Sidenav = () => {
               <AiOutlineHome size={20} />
             </a>
             <a
+              href="#aboutme"
+              className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+            >
+              <BsPerson size={20} />
+            </a>
+            <a
               href="#skill"
               className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
             >
@@ -84,12 +92,7 @@ const Sidenav = () => {
             >
               <AiOutlineProject size={20} />
             </a>
-            <a
-              href="#main"
-              className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
-            >
-              <BsPerson size={20} />
-            </a>
+
             <a
               href="#contact"
               className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
